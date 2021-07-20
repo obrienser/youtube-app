34f8348c3de73ef46376850f19d4ae5d853fdb34
Obrienser <obrienser@gmail.com>
2021-07-20 04:48:49 -0700

    Create the project

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Сергей Масленников on 20.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
