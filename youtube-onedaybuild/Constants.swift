06863d2278b0c4df88c31205077dff19d7ba20dd
Obrienser <obrienser@gmail.com>
2021-07-21 01:21:29 -0700

    parse JSON (errors)

--- youtube-onedaybuild/Constants.swift
+++ youtube-onedaybuild/Constants.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class Constants {
     
-    static var API_KEY = "AIzaSyCVWf7O_bBUyTegKNUNiePhGBloY28HWbU"
+    static var API_KEY = ""
     static var PLAYLIST_ID = "PL5hq3tApc7D7RYIT-HL2Yw1d7i9-3bqgz"
     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     
