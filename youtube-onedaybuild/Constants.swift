30c5697db486fd145d698b706f41891baa79f84b
Obrienser <obrienser@gmail.com>
2021-07-21 00:08:48 -0700

    Add API

---
+++ youtube-onedaybuild/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  youtube-onedaybuild
+//
+//  Created by Сергей Масленников on 20.07.2021.
+//
+
+import Foundation
+
+class Constants {
+    
+    static var API_KEY = "AIzaSyCVWf7O_bBUyTegKNUNiePhGBloY28HWbU"
+    static var PLAYLIST_ID = "PL5hq3tApc7D7RYIT-HL2Yw1d7i9-3bqgz"
+    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    
+}
---
+++ youtube-onedaybuild/Video.swift
@@ -0,0 +1,18 @@
+//
+//  Video.swift
+//  youtube-onedaybuild
+//
+//  Created by Сергей Масленников on 20.07.2021.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+    
+}
