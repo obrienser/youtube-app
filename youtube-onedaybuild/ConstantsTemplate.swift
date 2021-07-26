e5580f29e7cbdef92a4d94b95306dcc4c6dda083
Obrienser <obrienser@gmail.com>
2021-07-26 00:44:19 -0700

    stop trackeng Constants, make it safe

--- youtube-onedaybuild/ConstantsTemplate.swift
+++ youtube-onedaybuild/ConstantsTemplate.swift
@@ -1,16 +1,21 @@
 //
-//  Constants.swift
+//  ConstantsTemplate.swift
 //  youtube-onedaybuild
 //
-//  Created by Сергей Масленников on 20.07.2021.
+//  Created by Сергей Масленников on 26.07.2021.
 //
+/*
+ Add your own keys below and uncomment the code
+ */
 
 import Foundation
 
+/*
 class Constants {
     
     static var API_KEY = ""
-    static var PLAYLIST_ID = "PL5hq3tApc7D7RYIT-HL2Yw1d7i9-3bqgz"
+    static var PLAYLIST_ID = ""
     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     
 }
+*/
